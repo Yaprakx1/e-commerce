133a28b2bdc57ed02b6ea29053125a7b689afe20
Yaprakx1 <yprkiper@gmail.com>
2023-07-09 14:18:21 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Header from './components/Header'
+
+import MainPage from './pages/MainPage'
+
+function App() {
+  
+
+  return (
+    <>
+     <Header/>
+      <MainPage/>     
+    </>
+  )
+}
+
+export default App
